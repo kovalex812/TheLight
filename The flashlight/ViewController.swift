d5d4835116a822fe87245f23063c730197ee1418
Alexandr Kovalev <krollex.and.one@gmail.com>
2021-05-22 17:22:24 +0300

    Initial Commit

---
+++ The flashlight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The flashlight
+//
+//  Created by Alex Kovalev on 22.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
